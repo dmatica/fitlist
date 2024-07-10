4c01af9e708dd8be8f5fdbdbb8db06134b649bd8
Davinder Sandhu <dmatica@users.noreply.github.com>
2024-07-09 22:27:06 -0400

    Update AppleMusicMerge.py

--- Step1/AppleMusicMerge.py
+++ Step1/AppleMusicMerge.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from datetime import datetime
 desired_timezone = 'America/New_York'
+music_history_file = 'Apple Music Play Activity.csv'
+music_desc_file = 'Apple Music - Play History Daily Tracks.csv'
 
 def convert_numerical_to_date(numerical_date):
     # Reads in numerical_date value and returns as a datetime object
@@ -22,12 +24,10 @@ def split_dataframe_by_year(df):
     return dfs_by_year, unique_years
 
 # Read music history data
-music_history_file = 'Apple Music Play Activity.csv'
 music_history_cols = ["Event End Timestamp","Event Start Timestamp","Song Name", "UTC Offset In Seconds"]
 df_music_history = pd.read_csv(music_history_file, usecols=music_history_cols, parse_dates=['Event Start Timestamp','Event End Timestamp'])
 
 # Read music history track description data
-music_desc_file = 'Apple Music - Play History Daily Tracks.csv'
 music_desc_cols = ["Date Played", "Track Description"]
 df_music_desc = pd.read_csv(music_desc_file, usecols=music_desc_cols)
 
@@ -42,25 +42,20 @@ x = x.dropna(subset=['Event End Timestamp', 'Event Start Timestamp']).sort_value
 x['Track Description'] = ''
 count=0
 for index, row in x.iterrows():
-
-    #
     count=count+1
     song = str(row['Song Name'])
     date = row['Event Start Timestamp'].date()
-    #print(song,date)
     for index2, row2 in df_music_desc.iterrows():
         song_title = str(row2['Track Description'])
         date2 = row2['Date']
         if date==date2 and song in song_title:
-            #print(song,date,song_title)
             x.at[index, 'Track Description'] = song_title
-#    print(index)
 
-#print(x, x.columns)
+# Output new dataframe to general AppleMusicTrackData.csv file in the working directory
 x.to_csv('AppleMusicTrackData.csv',encoding='utf-8', index=False)
 
 
-
+# Split this into additional output files based on year
 dfs_by_year, unique_years = split_dataframe_by_year(x)
 for year in unique_years:
     print(dfs_by_year[year])
